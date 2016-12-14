f7dde68bdaac7bbe1b0d8a7afb5ffaffb18c3ffe
Niko-O <niko.o.generic@gmx.at>
2016-12-14 12:43:23 +0000

    Fixed Gathering UI not loading resources when run from container project
    When the Gathering.UI.Application is created from outside, resources
    cannot be resolved properly. Changed the code to call the Main method
    directly, instead.

--- TwitterTracks.Gathering/TwitterTracks.Gathering/Program.vb
+++ TwitterTracks.Gathering/TwitterTracks.Gathering/Program.vb
@@ -1,10 +1,11 @@
 ﻿Public Class Program
 
     Public Shared Sub Main(Args As String())
-
-        Dim App As New TwitterTracks.Gathering.UI.Application
-        App.StartupUri = New Uri("pack://application:,,,/TwitterTracks.Gathering.UI;component/MainWindow.xaml", UriKind.Absolute)
-        App.Run()
+        TwitterTracks.TweetinviInterop.ServiceProvider.RegisterService(TweetinviService.Instance)
+        TwitterTracks.Gathering.UI.Application.Main()
+        'Dim App As New TwitterTracks.Gathering.UI.Application
+        'App.StartupUri = New Uri("pack://application:,,,/TwitterTracks.Gathering.UI;component/MainWindow/MainWindow.xaml", UriKind.Absolute)
+        'App.Run()
     End Sub
 
 End Class
